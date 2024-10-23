4d95121bcc97fcfecb63af6ddbfb0b49022b85d6
TRISHA-JU-387 <satrisha123@gmail.com>
2024-10-23 15:25:15 +0600

    Update HomePage.java

--- HomePage.java
+++ HomePage.java
@@ -1,5 +1,3 @@
-
-
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
@@ -12,7 +10,7 @@ public class HomePage extends JFrame {
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         // Load the image
-        ImageIcon originalIcon = new ImageIcon("C:\\Users\\USER\\Downloads\\IMG-20240917-WA0004.jpg");  // Path to your image
+        ImageIcon originalIcon = new ImageIcon("C:\\Users\\DELL\\OneDrive\\Pictures\\645b00ff-a4f6-4b14-b70e-b37b53569c15.jpg");  // Path to your image
         Image originalImage = originalIcon.getImage();
 
         // Create a panel with custom painting
@@ -28,22 +26,21 @@ public class HomePage extends JFrame {
 
         // Create the welcome label
         JLabel welcomeLabel = new JLabel("Welcome to Online Voting System", SwingConstants.CENTER);
-        welcomeLabel.setFont(new Font("Serif", Font.BOLD, 50));  // Adjust the font size and style
+        welcomeLabel.setFont(new Font("Arial", Font.BOLD, 40));  // Adjust the font size and style
         welcomeLabel.setForeground(Color.BLACK);  // Set the text color to white for better visibility
         welcomeLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
 
         // Create the enter button
         JButton enterButton = new JButton("Press Enter");
-        enterButton.setAlignmentX(Component.CENTER_ALIGNMENT);
-        enterButton.setBackground(Color.WHITE);
-        enterButton.setFont(new Font("Serif", Font.PLAIN, 22));
-        enterButton.setMaximumSize(new Dimension(200, 100)); // Set button size
+        enterButton.setAlignmentX(Component.BOTTOM_ALIGNMENT);
+        enterButton.setMaximumSize(new Dimension(200, 50)); // Set button size
         enterButton.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
                 dispose();
                 new MenuPage().setVisible(true);
             }
         });
+         
 
         // Add spacing and components to the background panel
         backgroundPanel.add(Box.createVerticalGlue());  // Push contents to the center
